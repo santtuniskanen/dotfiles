929ff09b450c5eb34764d3b2087b4cdfb9528b0e
Santtu Niskanen <santtuniska02@gmail.com>
2025-11-10 20:46:32 +0200

    cleaned up nvim-notes init.lua file

--- nvim-notes/init.lua
+++ nvim-notes/init.lua
@@ -1,7 +1,5 @@
--- ~/.config/nvim-notes/init.lua
 vim.cmd.colorscheme('rose-pine-dawn')
 
--- Basic settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
@@ -12,11 +10,9 @@ vim.opt.linebreak = true
 vim.opt.spell = true
 vim.opt.spelllang = 'en_us'
 
--- Set leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -30,39 +26,32 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Install plugins
 require("lazy").setup({
-  -- Vimwiki for note-taking
   {
     "vimwiki/vimwiki",
     init = function()
       vim.g.vimwiki_list = {
         {
-          path = '~/notes/',
+          path = '~/Notes/',
           syntax = 'markdown',
           ext = '.md',
         }
       }
-      vim.g.vimwiki_global_ext = 0  -- Don't treat all .md files as vimwiki
+      vim.g.vimwiki_global_ext = 0 
     end,
   },
-
-  -- Telescope for fuzzy finding notes
   {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('telescope').setup{}
       
-      -- Keymaps
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
     end,
   },
-
-  -- Markdown preview (optional)
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
@@ -72,8 +61,6 @@ require("lazy").setup({
     end,
     ft = { "markdown" },
   },
-
-  -- Better markdown syntax
   {
     "preservim/vim-markdown",
     ft = "markdown",
@@ -84,9 +71,7 @@ require("lazy").setup({
   },
 })
 
--- Additional keymaps
 vim.keymap.set('n', '<leader>ww', ':VimwikiIndex<CR>', { desc = 'Open wiki index' })
 vim.keymap.set('n', '<leader>wt', ':VimwikiTable<CR>', { desc = 'Create table' })
 
--- Auto-create notes directory if it doesn't exist
-vim.fn.mkdir(vim.fn.expand('~/notes'), 'p')
+vim.fn.mkdir(vim.fn.expand('~/Notes'), 'p')
